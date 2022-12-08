13cb04cfdccd2a343d74f97e559a73a46e2e3dc1
Adema <adema.sharipova@inbox.ru>
2022-12-08 22:01:34 +0600

    lab7 2

--- lab7/lab7.sql
+++ lab7/lab7.sql
@@ -107,12 +107,13 @@ begin
 end;
 $$;
 
-create table timestamp_worker(
+create table timestamp_worker
+(
     time_action timestamp
 );
 
 create trigger timestamp_worker_trigger
-    before insert or update or delete
+    after insert or update or delete
     on worker
     for each row
 execute procedure timestamp_worker();
@@ -120,14 +121,99 @@ execute procedure timestamp_worker();
 insert into worker (name, date_of_birth, age, salary, work_experience, discount)
 VALUES ('name4', '12-12-2022', 18, 100000, 2, 0.15);
 
-update worker set name = 'newName4' where id = 4;
+update worker
+set name = 'newName4'
+where id = 4;
 
-select * from worker;
-select * from timestamp_worker;
+select *
+from worker;
+select *
+from timestamp_worker;
+
 
 -- b) Computes the age of a person when persons’date of birth is inserted.
 
+create or replace function compute_age()
+    returns trigger
+    language plpgsql
+as
+$$
+begin
+    new.age = date_part('year', age(current_date, new.date_of_birth));
+    return new;
+end;
+$$;
 
 
+create trigger compute_age_trigger
+    before insert
+    on worker
+    for each row
+execute procedure compute_age();
 
+insert into worker (name, date_of_birth, salary, work_experience, discount)
+VALUES ('name5', '2001-10-12', 100000, 2, 0.15);
+
+select *
+from worker;
+
+-- c) Adds 12% tax on the price of the inserted item.
+
+create table item
+(
+    id    serial primary key,
+    name  varchar,
+    price float
+);
+
+create or replace function add_tax()
+    returns trigger
+    language plpgsql
+as
+$$
+begin
+    new.price := cast((new.price * 0.12 + new.price) as float);
+    raise notice 'now, new price is %', new.price;
+    return new;
+end;
+$$;
+
+create trigger add_tax_trigger
+    before insert
+    on item
+    for row
+execute procedure add_tax();
+
+insert into item (name, price)
+VALUES ('name2', 23423);
+
+select *
+from item;
+
+-- d) prevents deletion of any row from only one table.
+
+create or replace function stop_deletion()
+    returns trigger
+    language plpgsql
+as
+$$
+begin
+    raise notice 'deletion stopped';
+    return null;
+end;
+$$;
+
+
+create trigger stop_deletion_trigger
+    before delete
+    on item
+    for each row
+execute procedure stop_deletion();
+
+drop trigger stop_deletion_trigger on item;
+
+delete from item where id = 2;
+select * from item;
+
+-- d) Launches functions 1.d and 1.e.
 
